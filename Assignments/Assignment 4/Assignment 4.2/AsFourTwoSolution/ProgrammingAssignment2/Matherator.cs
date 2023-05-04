63fef10fc716ce372429cb1c830754db7c8beca8
thientm-denk <denkhotieu@gmail.com>
2023-05-05 00:16:07 +0700

    assignment 4.2

---
+++ Assignments/Assignment 4/Assignment 4.2/AsFourTwoSolution/ProgrammingAssignment2/Matherator.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingAssignment2
+{
+    /// <summary>
+    /// Provides a variety of numeric methods
+    /// </summary>
+    public class Matherator
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public Matherator()
+        {
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Prints the numbers from 1 to 10
+        /// </summary>
+        public void PrintOneToTen()
+        {
+            PrintMToN(1, 10);
+        }
+
+        /// <summary>
+        /// Prints the numbers from m to n
+        /// </summary>
+        /// <param name="m">m</param>
+        /// <param name="n">n</param>
+        public void PrintMToN(int m, int n)
+        {
+            for (int i = m; i <= n; i++)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// Returns the tenth even number, with 2 as the first even number
+        /// </summary>
+        /// <returns>tenth even number</returns>
+        public int GetTenthEvenNumber()
+        {
+            return GetNthEvenNumber(10);
+        }
+
+        /// <summary>
+        /// Returns the nth even number, with 2 as the first even number
+        /// </summary>
+        /// <param name="n">n</param>
+        /// <returns>nth even number</returns>
+        public int GetNthEvenNumber(int n)
+        {
+            int evenCount = 0;
+            int i = 0;
+            while (evenCount < n)
+            {
+                i++;
+                if (i % 2 == 0)
+                {
+                    evenCount++;
+                }
+            }
+            return i;
+        }
+
+        #endregion
+    }
+}
---
+++ Assignments/Assignment 4/Assignment 4.2/AsFourTwoSolution/ProgrammingAssignment2/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace ProgrammingAssignment2
+{
+    // Don't change ANY of the code in this file; if you
+    // do, you'll break the automated grader!
+
+    /// <summary>
+    /// Programming Assignment 2
+    /// </summary>
+    class Program
+    {
+        // operation and arguments
+        static int operation;
+        static int firstArgument;
+        static int secondArgument;
+
+        /// <summary>
+        /// Tests the Matherator class
+        /// </summary>
+        /// <param name="args">command-line args</param>
+        static void Main(string[] args)
+        {
+            Matherator matherator = new Matherator();
+
+            // loop while there's more input
+            string input = Console.ReadLine();
+            while (input[0] != 'q')
+            {
+                // extract input values from string
+                GetInputValuesFromString(input);
+
+                // execute selected operation
+                if (operation == 1)
+                {
+                    Console.WriteLine(matherator.GetNthEvenNumber(firstArgument) + " ");
+                }
+                else if (operation == 2)
+                {
+                    Console.WriteLine(matherator.GetTenthEvenNumber() + " ");
+                }
+                else if (operation == 3)
+                {
+                    matherator.PrintMToN(firstArgument, secondArgument);
+                }
+                else
+                {
+                    matherator.PrintOneToTen();
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+
+        /// <summary>
+        /// Extracts the operation and arguments from the given input string
+        /// </summary>
+        /// <param name="input">input string</param>
+        static void GetInputValuesFromString(string input)
+        {
+            // extract operation
+            int spaceIndex = input.IndexOf(' ');
+            operation = int.Parse(input.Substring(0, spaceIndex));
+
+            // move along string and extract first argument
+            input = input.Substring(spaceIndex + 1);
+            spaceIndex = input.IndexOf(' ');
+            firstArgument = int.Parse(input.Substring(0, spaceIndex));
+
+            // second argument is the rest of the string
+            input = input.Substring(spaceIndex + 1);
+            secondArgument = int.Parse(input);
+        }
+    }
+}
